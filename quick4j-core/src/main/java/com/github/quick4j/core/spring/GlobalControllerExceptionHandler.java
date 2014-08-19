5b9a080af0664ab3c2a5b29fbcd8baa38af751b3
loafer <zjh527@163.com>
2014-08-19 16:23:08 +0800

    add

--- quick4j-core/src/main/java/com/github/quick4j/core/spring/GlobalControllerExceptionHandler.java
+++ quick4j-core/src/main/java/com/github/quick4j/core/spring/GlobalControllerExceptionHandler.java
@@ -1,7 +1,23 @@
 package com.github.quick4j.core.spring;
 
+import com.github.quick4j.core.web.http.AjaxResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
 /**
- * Created by zhaojh on 14-8-19.
+ * @author zhaojh
  */
+@ControllerAdvice
 public class GlobalControllerExceptionHandler {
+    @ExceptionHandler(Exception.class)
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public AjaxResponse processException(HttpServletRequest request, Exception ex){
+        String url = request.getRequestURL().toString();
+        String message = ex.getMessage();
+        return new AjaxResponse(AjaxResponse.Status.ERROR, message, url);
+    }
 }
