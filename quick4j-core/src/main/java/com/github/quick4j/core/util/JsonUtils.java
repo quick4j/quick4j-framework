8794794888ad5cfbf9b490d1a80c27df2f6c683e
loafer <zjh527@163.com>
2014-09-14 21:53:51 +0800

    扩展

--- quick4j-core/src/main/java/com/github/quick4j/core/util/JsonUtils.java
+++ quick4j-core/src/main/java/com/github/quick4j/core/util/JsonUtils.java
@@ -1,9 +1,11 @@
 package com.github.quick4j.core.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * @author zhaojh
@@ -23,6 +25,14 @@ public final class JsonUtils {
     public static <T> T formJson(String value, Class<T> valueType){
         try {
             return mapper.readValue(value, valueType);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T> T formJson(String value, TypeReference typeReference){
+        try {
+            return mapper.readValue(value, typeReference);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
