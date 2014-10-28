2768dce71eda226a54c46e0144534dbffaf34721
loafer <zjh527@163.com>
2014-10-28 17:50:22 +0800

    修改返回结果

--- quick4j-core/src/main/java/com/github/quick4j/core/spring/GlobalControllerExceptionHandler.java
+++ quick4j-core/src/main/java/com/github/quick4j/core/spring/GlobalControllerExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.github.quick4j.core.spring;
 
 import com.github.quick4j.core.web.http.AjaxResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,12 +14,18 @@ import javax.servlet.http.HttpServletRequest;
  */
 @ControllerAdvice
 public class GlobalControllerExceptionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(GlobalControllerExceptionHandler.class);
+
     @ExceptionHandler(Exception.class)
 //    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
     public AjaxResponse processException(HttpServletRequest request, Exception ex){
         String url = request.getRequestURL().toString();
         String message = ex.getMessage();
-        return new AjaxResponse(AjaxResponse.Status.ERROR, message, url);
+
+        logger.error("error: " + url, ex);
+        ex.printStackTrace();
+
+        return new AjaxResponse(AjaxResponse.Status.ERROR, message, url, request.getMethod());
     }
 }
