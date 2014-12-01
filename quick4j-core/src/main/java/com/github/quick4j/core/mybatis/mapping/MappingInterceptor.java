c1754fe87d5d0953a91f2e19df28c19c8ca1df8f
loafer <zjh527@163.com>
2014-12-01 16:50:52 +0800

    add auto create sql mapper

---
+++ quick4j-core/src/main/java/com/github/quick4j/core/mybatis/mapping/MappingInterceptor.java
@@ -0,0 +1,64 @@
+package com.github.quick4j.core.mybatis.mapping;
+
+import com.github.quick4j.core.mybatis.mapping.builder.MappedStatementAssistant;
+import org.apache.ibatis.binding.MapperMethod;
+import org.apache.ibatis.builder.annotation.ProviderSqlSource;
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+/**
+ * @author zhaojh.
+ */
+@Intercepts({
+    @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class}),
+    @Signature(type = Executor.class, method = "update", args = {MappedStatement.class,Object.class})
+})
+public class MappingInterceptor implements Interceptor{
+    private static Logger logger = LoggerFactory.getLogger(MappingInterceptor.class);
+
+    private static final int MAPPED_STATEMENT_INDEX = 0;
+    private static final int PARAMETER_INDEX = 1;
+    private static final int ROWBOUNDS_INDEX = 2;
+    private static final int RESULT_HANDLER_INDEX = 3;
+
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        final Object[] queryArgs = invocation.getArgs();
+        final MappedStatement ms = (MappedStatement) queryArgs[MAPPED_STATEMENT_INDEX];
+        final Object parameter = queryArgs[PARAMETER_INDEX];
+        final String sql = ms.getBoundSql(parameter).getSql();
+
+        logger.info("===> statement id: {}", ms.getId());
+        logger.info("===> parameter: {}", parameter);
+        logger.info("===> sql: {}", sql);
+
+        if(ms.getSqlSource() instanceof ProviderSqlSource){
+            switch (ms.getSqlCommandType()){
+                case SELECT:
+                    MappedStatement newStatement = MappedStatementAssistant.newSelectMappedStatement(ms, (Class) ((MapperMethod.ParamMap) parameter).get("type"));
+                    queryArgs[MAPPED_STATEMENT_INDEX] = newStatement;
+                    break;
+            }
+        }
+
+        return invocation.proceed();
+    }
+
+    @Override
+    public Object plugin(Object target) {
+        if(target instanceof Executor){
+            return Plugin.wrap(target, this);
+        }
+        return target;
+    }
+
+    @Override
+    public void setProperties(Properties properties) {}
+}
---
+++ quick4j-core/src/main/java/com/github/quick4j/core/mybatis/mapping/builder/EntityPersistentInfo.java
@@ -0,0 +1,108 @@
+package com.github.quick4j.core.mybatis.mapping.builder;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.persistence.Column;
+import javax.persistence.Table;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author zhaojh.
+ */
+public class EntityPersistentInfo {
+    private static final Logger logger = LoggerFactory.getLogger(EntityPersistentInfo.class);
+    private String tableName;
+    private Class entityClass;
+    private List<MappedColumn> mappedColumns = new ArrayList<MappedColumn>();
+    private String[] columns = null;
+
+    EntityPersistentInfo(Class entityClass) {
+        this.entityClass = entityClass;
+        parse();
+    }
+
+    public class MappedColumn{
+        private String name;
+        private String property;
+        private Class javaType;
+
+        public MappedColumn(String name, String property, Class javaType) {
+            this.name = name;
+            this.property = property;
+            this.javaType = javaType;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getProperty() {
+            return property;
+        }
+
+        public Class getJavaType() {
+            return javaType;
+        }
+
+        public Object getValue(Object entity){
+            Object value = null;
+            try {
+                Field field = entity.getClass().getDeclaredField(property);
+                field.setAccessible(true);
+                value = field.get(entity);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return "{" +
+                    "name='" + name + '\'' +
+                    ", property='" + property + '\'' +
+                    '}';
+        }
+    }
+
+    public String getTableName() {
+        if(null != tableName) return tableName;
+
+        if(!entityClass.isAnnotationPresent(Table.class)){
+            throw new RuntimeException("没有找到对应的物理表");
+        }
+
+        Table table = (Table) entityClass.getAnnotation(Table.class);
+        tableName = table.name();
+        return tableName;
+    }
+
+    public String[] getColumns(){
+        return columns;
+    }
+
+    public List<MappedColumn> getMappedColumns(){
+        return mappedColumns;
+    }
+
+    private void parse(){
+        List<String> columnNames = new ArrayList<String>();
+
+        Field[] fields = entityClass.getDeclaredFields();
+        for(Field field : fields){
+            if(!field.isAnnotationPresent(Column.class)){
+                continue;
+            }
+
+            Column column = field.getAnnotation(Column.class);
+            mappedColumns.add(new MappedColumn(column.name(), field.getName(), field.getType()));
+            columnNames.add(column.name());
+        }
+
+        columns = columnNames.toArray(new String[]{});
+    }
+}
